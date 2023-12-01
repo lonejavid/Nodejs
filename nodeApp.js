633821d4750104e7ae62d2146d64b61ed206913b
lonejavid <lonejavid0739@gmail.com>
2023-12-01 12:20:58 +0530

    based on the url hits it returns different responses

--- nodeApp.js
+++ nodeApp.js
@@ -1,6 +1,38 @@
-const http=require('http');
+const http= require('http');
 const server=http.createServer((req,res)=>{
-    console.log("JAVID")
-    
+
+const url=req.url;
+
+ if(url==='/home')
+{
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1> Welcome to Homepage</h1></body>');
+    res.write('<html>');
+   return res.end();
+}
+else if(url==='/about')
+{
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body> Welcome to About us Page</body>');
+    res.write('<html>');
+   return res.end();
+}
+else if(url==='/node')
+{
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body> Welcome to my Nodejs Project</body>');
+    res.write('<html>');
+   return res.end();
+}
+
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My first page</title></head>');
+res.write('<body><h1> this is from node app</body>');
+res.write('<html>');
+res.end();
 })
-server.listen(5000,'localhost');
+server.listen(3000)
